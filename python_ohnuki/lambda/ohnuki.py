ef93934414c0b8128a53aa57c476bed0942e29b6
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-01-18 22:56:03 +0900

    Lambdaで色々探り中(クソコミットメッセージ)

--- python_ohnuki/lambda/ohnuki.py
+++ python_ohnuki/lambda/ohnuki.py
@@ -5,17 +5,23 @@ from bs4 import BeautifulSoup
 import boto3
 
 s3 = boto3.resource('s3')
-
+    
 def lambda_handler(event, context):
     get_internet()
-
-
+    # file = get_internet()
+    # print('unko')
+    # upload_test(file)
+    
 def get_internet():
-    // 後で考える
-    url = "www.xxxyyyzzz.com"
+    url = "https://www.yahoo.co.jp/"
+    # yahooに対してgetリクエストを投げる
     response = requests.get(url)
     print(response)
     html = response.text
     print(html)
     soup = BeautifulSoup(html, 'html.parser')
+    return soup
 
+def upload_test(file):
+    bucket = s3.Bucket('unko-sample')
+    bucket.upload_file('sample.html', file)
