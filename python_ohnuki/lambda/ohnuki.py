776ab66845ed36604a5e899913e7616356e447a8
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-02-13 23:24:53 +0900

    レイヤの作成してライブラリ動くようになったところまで

--- python_ohnuki/lambda/ohnuki.py
+++ python_ohnuki/lambda/ohnuki.py
@@ -1,27 +1,44 @@
 import json
 import requests
-from bs4 import BeautifulSoup
+# from import BeautifulSoup
+import datetime
+
 
 import boto3
 
 s3 = boto3.resource('s3')
     
 def lambda_handler(event, context):
-    get_internet()
+    print('unko')
+    data = get_internet()
+    # print(file)
+    upload_test(data)
     # file = get_internet()
     # print('unko')
     # upload_test(file)
     
 def get_internet():
-    url = "https://www.yahoo.co.jp/"
-    # yahooに対してgetリクエストを投げる
+    url = "http://michaelsan.livedoor.biz/"
+    # urlに対してgetリクエストを投げる
     response = requests.get(url)
-    print(response)
+    # print(response)
     html = response.text
-    print(html)
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
+    return html
+    # print(html)
+    # soup = BeautifulSoup(html, 'html.parser')
+    # return soup
 
-def upload_test(file):
+def upload_test(data):
+    today = datetime.date.today().strftime('%Y%m%d')
+    print(today)
+    # tmpを入れないとエラー
+    file = open('/tmp/' + today + '.txt', 'w')
+    file.write(data)
+    file.close()
+
+    # s3 = boto3.resource('s3')
+    
+    # for bucket in s3.buckets.all():
+    #     print(bucket.name)
     bucket = s3.Bucket('unko-sample')
-    bucket.upload_file('sample.html', file)
+    bucket.upload_file("test/", file)
