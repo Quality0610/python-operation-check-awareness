54035f2930dfe43cb0bcd7f506dacd03c7fa3a8b
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-03-12 23:56:01 +0900

    databaseの設定

--- test_sns/sns_project/settings.py
+++ test_sns/sns_project/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'sns_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres',
+       'HOST': 'sns_db',
+       'PORT': 5432,
     }
 }
 
