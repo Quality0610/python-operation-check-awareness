f96fce7257450e3a6796aa8e79fc6506397589e5
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-03-21 15:21:29 +0900

    Userオブジェクトを使用してUserの取得

--- test_sns/sns_app/views.py
+++ test_sns/sns_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signupfunc(request):
+    user2 = User.objects.all()
+    print(user2)
+    print(request.method)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '',password)
+        print(request.POST)
+        return render(request, 'signup.html', {'some':100})
     return render(request, 'signup.html', {'some':100})
