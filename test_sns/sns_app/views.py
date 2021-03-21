c03dd2968fd538f9cac38b0f2363bca310f828dc
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-03-21 17:22:14 +0900

    User二重登録判定の追加

--- test_sns/sns_app/views.py
+++ test_sns/sns_app/views.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def signupfunc(request):
-    user2 = User.objects.all()
-    print(user2)
-    print(request.method)
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username, '',password)
-        print(request.POST)
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error':'このユーザは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '',password2)
+            print(request.POST)
         return render(request, 'signup.html', {'some':100})
     return render(request, 'signup.html', {'some':100})
